35990bd0a2bc6e0d8fb8ee0a5c9b977c629cdc20
Hanif Faiz Hidayat <grey.hanif@gmail.com>
2024-03-21 07:46:04 +0700

    serialization inclusion

--- src/test/java/programmerzamannow/json/FeatureTest.java
+++ src/test/java/programmerzamannow/json/FeatureTest.java
@@ -1,5 +1,6 @@
 package programmerzamannow.json;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.MapperFeature;
@@ -61,6 +62,19 @@ public class FeatureTest {
                 .configure(SerializationFeature.INDENT_OUTPUT, true);
         String json = objectMapper.writeValueAsString(person);
 
+        System.out.println(json);
+    }
+
+    @Test
+    void serializationInclusion() throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper()
+                .setSerializationInclusion(JsonInclude.Include.NON_NULL);
+
+        Person person = new Person();
+        person.setId("1");
+        person.setName("Hanif");
+
+        String json = objectMapper.writeValueAsString(person);
         System.out.println(json);
     }
 }
